59cf7b452bc6bd2e4291f2d39aa785e62d0f7517
hayabalasmeh <hmbalasmeh@yahoo.com>
2021-09-01 21:17:26 +0300

    fixing the test error

--- snacks/tests.py
+++ snacks/tests.py
@@ -14,83 +14,83 @@ class SnacksTest(TestCase):
     def test_string_representation(self):
         self.assertEqual(str(self.snack), "food")
 
-    # def test_list_page_status(self):
-    #     #Arrange
-    #     url = reverse('snack_list')
-    #     #Act
-    #     response = self.client.get(url)
-    #     #Assert
-    #     self.assertEqual(response.status_code, 200)
+    def test_list_page_status(self):
+        #Arrange
+        url = reverse('snack_list')
+        #Act
+        response = self.client.get(url)
+        #Assert
+        self.assertEqual(response.status_code, 200)
         
         
 
-    # def test_detail_page_status(self):
-    #     #Arrange
-    #     url = reverse('snack_detail', args='1')
-    #     #Act
-    #     response = self.client.get(url)
-    #     #Assert
-    #     self.assertEqual(response.status_code, 200)
+    def test_detail_page_status(self):
+        #Arrange
+        url = reverse('snack_detail', args='1')
+        #Act
+        response = self.client.get(url)
+        #Assert
+        self.assertEqual(response.status_code, 200)
 
-    # def test_list_page_templete(self):
-    #     #Arrange
-    #     url = reverse('snack_list')
-    #     #Act
-    #     response = self.client.get(url)
-    #     #Assert
-    #     self.assertTemplateUsed(response, 'snack_list.html')
-    #     self.assertContains(response, "food")
-    #     self.assertTemplateUsed(response, 'base.html')
+    def test_list_page_templete(self):
+        #Arrange
+        url = reverse('snack_list')
+        #Act
+        response = self.client.get(url)
+        #Assert
+        self.assertTemplateUsed(response, 'snacks/snack_list.html')
+        self.assertContains(response, "food")
+        self.assertTemplateUsed(response, '_base.html')
 
-    # def test_detail_page_templete(self):
-    #     #Arrange
-    #     url = reverse('snack_detail', args='1')
-    #     #Act
-    #     response = self.client.get(url)
-    #     #Assert
-    #     self.assertTemplateUsed(response, 'snack_detail.html')
-    #     self.assertContains(response, "Snack : food")
-    #     self.assertTemplateUsed(response, 'base.html')
+    def test_detail_page_templete(self):
+        #Arrange
+        url = reverse('snack_detail', args='1')
+        #Act
+        response = self.client.get(url)
+        #Assert
+        self.assertTemplateUsed(response, 'snacks/snack_detail.html')
+        self.assertContains(response, "Snack : food")
+        self.assertTemplateUsed(response, '_base.html')
     
-    # def test_snack_content(self):
-    #     self.assertEqual(f"{self.snack.title}", "food")
-    #     self.assertEqual(f"{self.snack.purchaser}", "tester@email.com")
-    #     self.assertEqual(f'{self.snack.description}', 'tasty food')
+    def test_snack_content(self):
+        self.assertEqual(f"{self.snack.title}", "food")
+        self.assertEqual(f"{self.snack.purchaser}", "tester@email.com")
+        self.assertEqual(f'{self.snack.description}', 'tasty food')
 
-    # def test_snack_create(self):
-    #     #Arrange
-    #     create_response = self.client.post(
-    #         reverse("snack_create"),
-    #         {
-    #             "title": "Healthy Oat Bar",
-    #             "description": "yummy",
-    #             "purchaser": self.user.id,
-    #         }, follow=True
-    #     )
+    def test_snack_create(self):
+        #Arrange
+        create_response = self.client.post(
+            reverse("snack_create"),
+            {
+                "title": "Healthy Oat Bar",
+                "description": "yummy",
+                "purchaser": self.user.id,
+            }, follow=True
+        )
         
-    #     #Act
-    #     url = reverse('snack_detail', args='2')
-    #     #Assert
-    #     self.assertRedirects(create_response ,url)
-    #     self.assertContains(create_response, "Healthy Oat Bar")
+        #Act
+        url = reverse('snack_detail', args='2')
+        #Assert
+        self.assertRedirects(create_response ,url)
+        self.assertContains(create_response, "Healthy Oat Bar")
 
-    # def test_snack_delete_view(self):
-    #     response = self.client.get(reverse("snack_delete", args="1"))
-    #     self.assertEqual(response.status_code, 200)
+    def test_snack_delete_view(self):
+        response = self.client.get(reverse("snack_delete", args="1"))
+        self.assertEqual(response.status_code, 200)
     
-    # def test_snack_update(self):
-    #     #Arrange
-    #     create_response = self.client.post(
-    #         reverse("snack_update", args='1'),
-    #         {
-    #             "title": "Healthy Granula Bar",
-    #             "description": "Extra yummy",
-    #             "purchaser": self.user.id,
-    #         }, follow=True
-    #     )
+    def test_snack_update(self):
+        #Arrange
+        create_response = self.client.post(
+            reverse("snack_update", args='1'),
+            {
+                "title": "Healthy Granula Bar",
+                "description": "Extra yummy",
+                "purchaser": self.user.id,
+            }, follow=True
+        )
         
-    #     #Act
-    #     url = reverse('snack_detail', args='1')
-    #     #Assert
-    #     self.assertRedirects(create_response ,url)
-    #     self.assertContains(create_response, "Healthy Granula Bar")
+        #Act
+        url = reverse('snack_detail', args='1')
+        #Assert
+        self.assertRedirects(create_response ,url)
+        self.assertContains(create_response, "Healthy Granula Bar")
